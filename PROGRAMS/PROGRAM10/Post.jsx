1c730903772e849819e1a26b6eaaf8ba9a6c1a1d
MRIDULA-0-9 <mridula09042006@gmail.com>
2026-02-22 13:43:48 +0530

    Add files via upload

---
+++ PROGRAMS/PROGRAM10/Post.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+function Post() {
+  return (
+    <div>
+      <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. 
+        Tempora veniam alias minima laboriosam cumque incidunt autem
+        dolorum illum unde expedita, fugiat beatae sint consectetur omnis, 
+        consequuntur iste nobis neque excepturi.</p>
+    </div>
+  )
+}
+
+export default Post
